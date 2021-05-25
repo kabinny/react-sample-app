9a5b9615e3f1d6b68692bc8a530945ed4530e3e8
kabinny <kabinny@gmail.com>
2021-05-25 21:40:43 +0900

    props and state

--- src/App.js
+++ src/App.js
@@ -7,11 +7,24 @@ import './App.css';
 // App.js에 쓰고 있는 코드는 유사자바스크립트이다. create-react-app이 js코드로 컨버트.
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: {title: 'WEB', sub: 'world wide web!'},
+      contents: [
+        {id: 1, title: 'HTML', desc: 'HTML is for information'},
+        {id: 2, title: 'CSS', desc: 'CSS is for design'},
+        {id: 3, title: 'Javascript', desc: 'Javascript is for interactive'},
+      ]
+    }
+  } // render 함수 전에 먼저 실행하고 싶은거는 여기에...초기화 담당
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <TOC></TOC>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}></Subject>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 
 class TOC extends Component {
   render() {
+    var lists = [];
+    var data = this.props.data;
+    var i = 0;
+    while(i < data.length) {
+      // 여러 요소를 자동으로 생성할 때는 고유의 key값이 있어야 한다. 리액트가 필요로 하는 것이니 그러려니 한다. 
+      lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>)
+      i = i + 1;
+    } 
     return (
       <nav>
         <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript</a></li>
+          {lists}
         </ul>
       </nav>
     );
