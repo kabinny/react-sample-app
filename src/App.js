03ad0fdd36862a803a053c7e23f2e479dae10e7b
kabinny <kabinny@gmail.com>
2021-06-05 23:15:59 +0900

    이벤트 state props, render 함수, 이벤트 설치

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode: 'read',
       subject: {title: 'WEB', sub: 'world wide web!'},
+      welcome: {title: 'Welcome', desc: 'Hello, React!'},
       contents: [
         {id: 1, title: 'HTML', desc: 'HTML is for information'},
         {id: 2, title: 'CSS', desc: 'CSS is for design'},
@@ -19,13 +21,29 @@ class App extends Component {
     }
   } // render 함수 전에 먼저 실행하고 싶은거는 여기에...초기화 담당
   render() {
+    console.log('App render');
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
-        <Subject 
+        {/* <Subject 
           title={this.state.subject.title} 
-          sub={this.state.subject.sub}></Subject>
+          sub={this.state.subject.sub}></Subject> */}
+        <header> 
+          <h1><a href="/" onClick={function(e){
+            console.log(e);
+            e.preventDefault(); // a tag의 기본 동작 막는다.
+          }}>{this.state.subject.title}</a></h1>
+          {this.state.subject.sub}
+        </header>
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
--- src/components/Subject.js
+++ src/components/Subject.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 
 class Subject extends Component { // Subject라는 컴포넌트를 만들겠다. 
   render() { // render라는 함수가 있어야 함. 클래스 안에서는 'function' 생략
+    console.log('Subject render');
     return ( // 하나의 최상위 태그로 시작해야함
      <header> 
-       <h1>{this.props.title}</h1>
+       <h1><a href="/">{this.props.title}</a></h1>
        {this.props.sub}
      </header>
     );
