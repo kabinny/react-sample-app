e7f264605eafb9278af5c754fd7b3ea798724b70
kabinny <kabinny@gmail.com>
2021-06-07 21:51:27 +0900

    컴포넌트 이벤트 만들기

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -8,7 +8,18 @@ class TOC extends Component {
     var i = 0;
     while(i < data.length) {
       // 여러 요소를 자동으로 생성할 때는 고유의 key값이 있어야 한다. 리액트가 필요로 하는 것이니 그러려니 한다. 
-      lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>)
+      lists.push(
+        <li key={data[i].id}>
+          <a 
+            href={"/content/"+data[i].id}
+            data-id={data[i].id}
+            onClick={function(id, e){
+              e.preventDefault();
+              this.props.onChangePage(id);
+            }.bind(this, data[i].id)}
+          >{data[i].title}</a>
+        </li>
+      );
       i = i + 1;
     } 
     return (
