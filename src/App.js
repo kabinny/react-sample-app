db3c578c5fcc0c65c9b091a11e48c89a26ccc2ed
kabinny <kabinny@gmail.com>
2021-06-22 20:27:07 +0900

    update - state 변경

--- src/App.js
+++ src/App.js
@@ -49,35 +49,35 @@ class App extends Component {
       _article = <CreateContent onSubmit={function(_title, _desc){
         // setState로 새로운 content값 추가 add content to this.state.contents
         this.max_content_id = this.max_content_id + 1;
-        // this.state.contents.push(
-        //   {id: this.max_content_id, title: _title, desc: _desc}
-        // );
-        var _contents = this.state.contents.concat(
-          {id: this.max_content_id, title: _title, desc: _desc}
-        ) // push는 원본 배열을 수정하기 때문에 나중을 위해 concat를 이용하자
 
-        // // push를 쓰려면 배열을 복제해서 쓰자!
-        // var newContents = Array.from(this.state.contents);
-        // newContents.push({id: this.max_content_id, title: _title, desc: _desc});
+        var _contents = Array.from(this.state.contents);
+        _contents.push({id: this.max_content_id, title: _title, desc: _desc});
 
         this.setState({
-          contents: _contents
+          contents: _contents,
+          mode:'read',
+          selected_content_id:this.max_content_id
         })
         console.log(_title, _desc);
       }.bind(this)}></CreateContent>;
     } else if(this.state.mode === 'update') {
       _content = this.getReadContent();
-      _article = <UpdateContent data={_content} onSubmit={function(_title, _desc){
-        this.max_content_id = this.max_content_id + 1;
-
-        var _contents = this.state.contents.concat(
-          {id: this.max_content_id, title: _title, desc: _desc}
-        ) 
-        this.setState({
-          contents: _contents
-        })
-        console.log(_title, _desc);
-      }.bind(this)}></UpdateContent>;
+      _article = <UpdateContent data={_content} onSubmit={
+        function(_id, _title, _desc){
+          var _contents = Array.from(this.state.contents);
+          var i = 0;
+          while(i < _contents.length){
+            if(_contents[i].id === _id) {
+              _contents[i] = {id:_id, title:_title, desc:_desc};
+              break;
+            }
+            i = i + 1;
+          }
+          this.setState({
+            contents: _contents,
+            mode:'read'
+          });
+        }.bind(this)}></UpdateContent>;
     }
     return _article;
   }
